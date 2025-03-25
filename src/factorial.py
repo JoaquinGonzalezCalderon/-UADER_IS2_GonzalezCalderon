5d5d25754de2b99b9391a6ace093bac6551dc618
Joaquin Gonzalez Calderon <startgame1975@gmail.com>
2025-03-25 10:17:47 -0300

    2da modificacion factorial.py

--- src/factorial.py
+++ src/factorial.py
@@ -7,9 +7,11 @@
 #*-------------------------------------------------------------------------*
 import sys
 
+import sys
+
 def factorial(num): 
     if num < 0: 
-        print("Factorial de un número negativo no existe")
+        print(f"Factorial de {num} no existe")
         return 0
     elif num == 0: 
         return 1
@@ -21,21 +23,33 @@ def factorial(num):
         return fact 
 
 if len(sys.argv) != 2:
-   print("Debe informar un rango de números en el formato desde-hasta (ej. 4-8)!")
-   sys.exit()
+    print("Debe informar un rango de números en los formatos: desde-hasta, -hasta o desde-")
+    sys.exit()
+
+arg = sys.argv[1]
 
 try:
-    # Separar los valores del rango
-    desde, hasta = map(int, sys.argv[1].split('-'))
-    
-    if desde > hasta:
-        print("El primer número debe ser menor o igual al segundo.")
-        sys.exit()
-    
-    for num in range(desde, hasta + 1):
-        print(f"Factorial {num}! es {factorial(num)}")
+    if '-' in arg:
+        if arg.startswith('-'):  # Caso "-hasta"
+            hasta = int(arg[1:])  # Convertir la parte numérica a entero
+            desde = 1
+        elif arg.endswith('-'):  # Caso "desde-"
+            desde = int(arg[:-1])  # Convertir la parte numérica a entero
+            hasta = 60
+        else:  # Caso "desde-hasta"
+            desde, hasta = map(int, arg.split('-'))
 
+        if desde > hasta:
+            print("El primer número debe ser menor o igual al segundo.")
+            sys.exit()
+
+        for num in range(desde, hasta + 1):
+            print(f"Factorial {num}! es {factorial(num)}")
+
+    else:
+        print("Formato incorrecto. Debe ingresar un rango válido como -10, 5-20 o 15-")
 except ValueError:
-    print("Formato incorrecto. Debe ingresar dos números separados por un guion (ej. 4-8).")
+    print("Error: Asegúrese de ingresar valores numéricos correctamente.")
+
 
 
